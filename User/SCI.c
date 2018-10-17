91d6c09ea06bca3db0f143012a9576cd60aaed89
NULL <lixiang3.1416@qq.com>
2018-10-17 19:07:04 +0800

    uart4 9600 initialization

--- User/SCI.c
+++ User/SCI.c
@@ -1,4 +1,4 @@
-﻿
+
 #include "main.H"
 #include "SCI.H"
 /*********************************************************************
@@ -165,3 +165,48 @@ void RS232_Send_Data(unsigned char *send_buff,unsigned int length)
 //   	  nCount --;   
 //   }
 // }
+
+
+void UART4_Configuration(void)
+{ 
+	
+	GPIO_InitTypeDef GPIO_InitStructure;//定义GPIO_InitTypeDef类型的结构体成员GPIO_InitStructure
+
+	USART_InitTypeDef USART_InitStructure;
+	USART_ClockInitTypeDef USART_ClockInitStruct;
+	//使能需要用到的GPIO管脚时钟
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	//使能USART1 时钟
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+	///复位串口1
+	USART_DeInit(UART4);
+	
+	USART_StructInit(&USART_InitStructure);//载入默认USART参数
+	USART_ClockStructInit(&USART_ClockInitStruct);//载入默认USART参数        
+	//配置串口1的管脚 PA8 USART1_EN PA9 USART1_TX PA10 USART1_RX    
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;    //复用
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; //推挽输出
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10; 
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_PinAFConfig(GPIOC,GPIO_PinSource10,GPIO_AF_UART4);        //管脚PA9复用为USART1
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;        
+	GPIO_Init(GPIOC, &GPIO_InitStructure);                                                                                                                 
+	GPIO_PinAFConfig(GPIOC,GPIO_PinSource11,GPIO_AF_UART4);
+	
+	USART_ClockInit(UART4,&USART_ClockInitStruct);
+
+
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(UART4,&USART_InitStructure); 
+
+	USART_ITConfig(UART4, USART_IT_RXNE, ENABLE);        ///////接收中断使能
+	USART_ClearITPendingBit(UART4, USART_IT_TC);//清除中断TC位
+	USART_Cmd(UART4,ENABLE);//最后使能串?
+}
